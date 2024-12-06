4892d80559b8bb5954783816c60b154732a32f05
codetrsh <134286416+codetrsh@users.noreply.github.com>
2024-12-06 19:38:18 -0300

    wip

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,29 +1,45 @@
-import { Controller, Get, Post, Patch, Delete, Param, Body } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Delete, Param, Body, Query, ParseIntPipe } from '@nestjs/common';
+import { UsersService } from './users.service';
+
+// interface user {username: string, email: string, role: 'INTERN' | 'ENGINEER' | 'ADMIN'}
+
+// all received params comes as strings so + to turn into numbers hehe
 
 @Controller('users') // this is a decorator /users
 export class UsersController {
-  // GET users 
-  @Get() // /users
-  findAll() {
-    return ['hi']
-  }
 
-  @Get('interns') //⚠️ = users/interns
-  findAllInterns() {
-    return []
+  constructor(private readonly usersService: UsersService){}
+
+  // The query is used inside the method not the decorator itself
+  // GET users 
+  @Get() // /users - using query params /users?role=value - Called decorators
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {  // those are class methods of the UsersController
+    return this.usersService.findAll(role) // sends this info to service file
   }
 
   // GET users/:id - If I have any other get route that uses users/<anything> it needs to be before this route, else this route will get all routes. if you have users/interns it has to be before the users/:id route ⚠️
   @Get(':id') // users/id
-  findOne(@Param('id') id: string){
-    return {id}
+  findOne(@Param('id', ParseIntPipe) id: number){ // parserInt will change the string from params to number
+    return this.usersService.findOne(+id)
   }
 
   // POST users
   @Post() 
-  create(@Body() user: {}){
-    return user 
+  create(@Body() user: {username: string, email: string, role: 'INTERN' | 'ENGINEER' | 'ADMIN'}){
+    console.log(user)
+
+    return this.usersService.create(user)
   }
-  // PATCH /users/:id
+  // PATCH /users/:id | in the patch since is update we have the id and we also receive a body with the update
+  @Patch(':id')
+  updateOne(@Param('id', ParseIntPipe) id: number, @Body() userUpdate: {username?: string; email?: string; role?: "INTERN" | "ENGINEER" | "ADMIN"}){
+    console.log(userUpdate)
+    return this.usersService.updateOne(id, userUpdate)
+  }
   // DELETE /users/:id
+  @Delete(':id')
+  deleteOne(@Param('id', ParseIntPipe) id: number){
+    console.log(id);
+    return this.usersService.deleteOne(id);
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,76 @@
 import { Injectable } from '@nestjs/common';
 
+interface User{
+  id: number,
+  username: string,
+  email: string,
+  role: 'INTERN' | 'ENGINEER' | 'ADMIN'
+}
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: User[] = [
+    {
+      "id": 1,
+      "username": "john",
+      "email": "john@example.com",
+      "role": "ADMIN"
+    },
+    {
+      "id": 2,
+      "username": "paul",
+      "email": "paul@example.com",
+      "role": "INTERN"
+    },
+    {
+      "id": 3,
+      "username": "saul",
+      "email": "saul@example.com",
+      "role": "ENGINEER"
+    }
+  ]
+
+  // filter users based on role, if role is not provided, returns all users
+  findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN'): {} {
+    if(role){
+      return this.users.filter(user => user.role === role) // referes to the private users above
+    }
+    return this.users;
+  }
+
+  findOne(id: number): any {
+    return this.users.find(user => user.id === id);
+  }
+
+  create(user: {username: string, email: string, role: 'INTERN' | 'ENGINEER' | 'ADMIN'})  {
+
+    const usersByHighestId = [...this.users].sort((a, b) => b.id - a.id);
+    // constructu the user to be updated and push it to the users array
+    const newUser = {
+      id: usersByHighestId[0].id + 1, // just to generate a id
+      ...user
+    }
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  // here the updated user is what we receive in the body for the post request
+  updateOne(id: number, updatedUser: { username?: string, email?: string, role?: 'INTERN' | 'ENGINEER' | 'ADMIN'}){
+    this.users = this.users.map(user => {
+      if(user.id === id){
+        return {...user,...updatedUser} // spread all the users props and merges the props from user and updatedUser
+      }
+      return user;
+    })
+    return this.findOne(id) // updates the user above and uses the findOne to find the updated user
+  }
+
+  // In RESTful APIs, it's often helpful to return the resource that was deleted, so the client can confirm which specific object was removed
+  deleteOne(id: number) {
+    const removedUser = this.findOne(id);
+
+    // updates the user array to be an array without the deleted user
+    this.users = this.users.filter(user => user.id !== id)
+    return removedUser;
+  }
+}
